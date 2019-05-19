3aeddf5a7d3e9b6d300c20d5ca1ddc0a14eae976
Silas Brumwell <silas.brumwell@autozone.com>
2019-05-18 21:22:00 -0500

    Rudimentary sign in button and start to sign in flow

--- app/src/main/java/com/tavarus/artabletop/MainActivity.kt
+++ app/src/main/java/com/tavarus/artabletop/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.tavarus.artabletop
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.MotionEvent
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
 import com.google.ar.core.HitResult
 import com.google.ar.core.Plane
 import com.google.ar.sceneform.AnchorNode
 import com.google.ar.sceneform.ux.ArFragment
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -14,11 +20,26 @@ class MainActivity : AppCompatActivity() {
     private var arFragment: ArFragment? = null
     private var board: Board? = null
 
+    private val RC_SIGN_IN = 516
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         arFragment = supportFragmentManager.findFragmentById(R.id.ux_fragment) as ArFragment?
+        arFragment!!.planeDiscoveryController.hide()
+        arFragment!!.planeDiscoveryController.setInstructionView(null)
+
+        signInButton.setOnClickListener {
+            val providers = arrayListOf(
+                AuthUI.IdpConfig.EmailBuilder().build(),
+                AuthUI.IdpConfig.GoogleBuilder().build())
+            startActivityForResult(
+                AuthUI.getInstance()
+                    .createSignInIntentBuilder()
+                    .setAvailableProviders(providers)
+                    .build(), RC_SIGN_IN)
+        }
 
         board = Board(6, 6, 0.15f, arrayOf(0, 1, 2, 6, 7, 21, 22, 32, 33, 33, 34),
             arrayOf(13, 14, 15, 16, 18, 19, 20, 33, 34) ,this)
@@ -33,7 +54,24 @@ class MainActivity : AppCompatActivity() {
             val boardNode = board!!.boardNode()
             anchorNode.addChild(boardNode)
         }
+    }
 
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            arFragment!!.planeDiscoveryController.hide()
+            val response = IdpResponse.fromResultIntent(data)
+
+            if (resultCode == Activity.RESULT_OK) {
+                val user = FirebaseAuth.getInstance().currentUser
+            } else {
+                // Sign in failed. If response is null the user canceled the
+                // sign-in flow using the back button. Otherwise check
+                // response.getError().getErrorCode() and handle the error.
+                // ...
+            }
+        }
     }
 }
