373d16a6eca328e5ab8945c669d24df098109a5a
Silas Brumwell <silas.brumwell@autozone.com>
2019-05-21 21:00:28 -0500

    Added firebase AuthUI and handle login

--- app/src/main/java/com/tavarus/artabletop/MainActivity.kt
+++ app/src/main/java/com/tavarus/artabletop/MainActivity.kt
@@ -4,7 +4,9 @@ import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.view.MotionEvent
+import android.view.View
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.IdpResponse
 import com.google.ar.core.HitResult
@@ -27,8 +29,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         arFragment = supportFragmentManager.findFragmentById(R.id.ux_fragment) as ArFragment?
-        arFragment!!.planeDiscoveryController.hide()
-        arFragment!!.planeDiscoveryController.setInstructionView(null)
 
         signInButton.setOnClickListener {
             val providers = arrayListOf(
@@ -40,8 +40,13 @@ class MainActivity : AppCompatActivity() {
                     .setAvailableProviders(providers)
                     .build(), RC_SIGN_IN)
         }
+        //Eventually this would be a full menu
+        settingsButton.setOnClickListener {
+            AuthUI.getInstance().signOut(this)
+                .addOnCompleteListener { unauthedUI() }
+        }
 
-        board = Board(6, 6, 0.15f, arrayOf(0, 1, 2, 6, 7, 21, 22, 32, 33, 33, 34),
+        board = Board(6, 6, 0.05f, arrayOf(0, 1, 2, 6, 7, 21, 22, 32, 33, 33, 34),
             arrayOf(13, 14, 15, 16, 18, 19, 20, 33, 34) ,this)
 
         arFragment!!.setOnTapArPlaneListener { hitResult: HitResult, plane: Plane, motionEvent: MotionEvent ->
@@ -54,6 +59,12 @@ class MainActivity : AppCompatActivity() {
             val boardNode = board!!.boardNode()
             anchorNode.addChild(boardNode)
         }
+
+        if (FirebaseAuth.getInstance().currentUser == null) {
+            unauthedUI()
+        } else {
+            authedUI()
+        }
     }
 
 
@@ -61,11 +72,11 @@ class MainActivity : AppCompatActivity() {
         super.onActivityResult(requestCode, resultCode, data)
 
         if (requestCode == RC_SIGN_IN) {
-            arFragment!!.planeDiscoveryController.hide()
             val response = IdpResponse.fromResultIntent(data)
-
             if (resultCode == Activity.RESULT_OK) {
+                //This is where we want to start getting the user's data, specifically saved boards
                 val user = FirebaseAuth.getInstance().currentUser
+                authedUI()
             } else {
                 // Sign in failed. If response is null the user canceled the
                 // sign-in flow using the back button. Otherwise check
@@ -73,5 +84,20 @@ class MainActivity : AppCompatActivity() {
                 // ...
             }
         }
+    }
+
+    fun authedUI() {
+        arFragment!!.planeDiscoveryController.show()
+        signInButton.visibility = View.GONE
+        settingsButton.visibility = View.VISIBLE
+        //It would be nice to see if we could add back the default planeDiscoveryController InstructionView
+        //(That's the little phone that moves around
+    }
+
+    fun unauthedUI() {
+        arFragment!!.planeDiscoveryController.hide()
+        signInButton.visibility = View.VISIBLE
+        settingsButton.visibility = View.GONE
+        arFragment!!.planeDiscoveryController.setInstructionView(null)
     }
 }
